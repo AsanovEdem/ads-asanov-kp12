a319fbbc63c6e40a90e7b29bfb8c9b13dc84f55b
AsanovEdem <asanovedem2585@gmail.com>
2021-10-20 15:19:22 +0300

    laboratory work completed

---
+++ laba-1/Task_1/Завдання №1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using static System.Console;
+using static System.Math;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Write("Enter x: ");
+            int x = Convert.ToInt32(ReadLine());
+            Write("Enter y: ");
+            int y = Convert.ToInt32(ReadLine());
+            Write("Enter z: ");
+            int z = Convert.ToInt32(ReadLine());
+
+            double atmp1, atmp2;
+
+            atmp1 = y - Sqrt(Abs(Pow(x, 3)));
+            atmp2 = Sqrt(Pow(x, 3) + 5 * Pow(y, -z) + Pow(z, 2));
+
+            double a = atmp1 / atmp2;
+            double b = Sin(Pow(a, -x)) + y;
+
+            WriteLine("a = " + a);
+            Write("b = " + b);
+        }
+    }
+}
---
+++ laba-1/Task_2/Завдання №2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using static System.Console;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = 0;
+            for (int Y = 1582; Y <= 4902; Y++)
+            {
+                int y = Y % 1000;
+                int c = Y / 1000;
+                for (int m = 1; m <= 12; m++)
+                {
+                    if (((2.6 * m - 0.2) + 13 + y + y / 4 + c / 4 - 2 * c) % 7 == 5)
+                        count++;
+                }
+            }
+            WriteLine("Количество пятниц 13: " + count.ToString());
+        }
+    }
+}
